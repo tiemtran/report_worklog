fbc5f22c02f05580f894f399995b0e425b7b99be
LongLD <hicongtuoc@gmail.com>
2025-01-02 19:52:27 +0700

    init project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: { // [!code ++] // [!code focus]
+    serverComponentsExternalPackages: ['grammy'], // [!code ++] // [!code focus]
+  }, // [!code ++] // [!code focus]
 };
 
 export default nextConfig;
