d129292ccc99a6fe863d927d20f357cfb1f3d6e8
Long Lê Đăng <hicongtuoc@gmail.com>
2025-01-03 10:36:24 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: { // [!code ++] // [!code focus]
-    serverComponentsExternalPackages: ['grammy'], // [!code ++] // [!code focus]
-  }, // [!code ++] // [!code focus]
+  serverExternalPackages: ['grammy'],
 };
 
 export default nextConfig;
